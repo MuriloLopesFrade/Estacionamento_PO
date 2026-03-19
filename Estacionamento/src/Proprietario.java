8891f30a8734caeb44b273a6b06ab5b1a0b40832
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-19 09:47:30 -0300

    Criação das classes e instrancias

---
+++ Estacionamento/src/Proprietario.java
@@ -0,0 +1,10 @@
+public class Proprietario {
+
+    String nome;
+    String cpf;
+
+    public Proprietario(String nome,String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+}
---
+++ Estacionamento/src/Veiculo.java
@@ -0,0 +1,21 @@
+public class Veiculo {
+
+    String marca;
+    String modelo;
+    String placa;
+    Proprietario proprietario;
+
+    public Veiculo(String marca,String modelo,String placa,Proprietario proprietario) {
+        this.modelo = modelo;
+        this.marca = marca;
+        this.placa = placa;
+        this.proprietario = proprietario;
+    }
+
+    public void entrada(){
+
+
+
+    }
+
+}
